a4c658b0f7c023070afc22deacc0e278e2bda320
unknown <joshstat126@gmail.com>
2026-04-18 15:16:46 +0800

    New "resolver" module

--- crates/carrier-core/src/lib.rs
+++ crates/carrier-core/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod carrier_toml;
+pub mod cran;          
 pub mod formats;
 pub mod manifest;
-pub mod paths;
 pub mod ops;
+pub mod paths;
+pub mod version;    
 
 pub use carrier_toml::CarrierToml;
 pub use manifest::Manifest;
-pub use paths::resolve_install_dir;
+pub use paths::{resolve_install_dir, resolve_r_lib_dir};
